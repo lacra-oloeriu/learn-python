0683a48cae140178d6a849c0ed1170cdf8d365c3
lacra-oloeriu <lacramioara.oloeriu@gmail.com>
2017-05-30 12:51:33 +0200

    my game

--- ex45.py
+++ ex45.py
@@ -21,6 +21,71 @@ class Engine(object):
             current_stage = self.stage_map.next_stage(next_stage_name)
         # be sure to print out the last stage
         current_stage.enter()
+class Price(Stage):
+    quips = [
+        " You finish the championchip in the 3 position.",
+        " Your Mom would be very proud of you. ",
+        " The result is amaising.",
+        " Because the competiotion was verry difficult.",
+        " Good job! ."
+
+    ]
+    def enter(self):
+        print(Price.quips[randint(0,len(self.quips) -1)])
+        exit(1)
+class Game(Stage):
+    def enter(self):
+        print(dedent("""
+             You start the conpetition in the 64 position
+             That isn't so nice for you.
+             You have to work hard.
+             After the first 3 ronds you are in the first 20 players.
+             and after one more 3 tyou are in the first 10.
+             At the finish line you are in the 3 position .Good job!
+             """))
+
+        action = input(">")
+
+        if action == "good!":
+            print(dedent("""
+                  This conpetition it's very hard for you
+                  You after do your best this year:
+                  Try to earn even 5 points ,
+                  and next year you can become campion
+                  In the mind time .
+                  Work hard every single day
+                  and try to do your best
+                  go go go.
+                  """))
+            return'iupii'
+
+        elif action == "game!":
+            print(dedent("""
+                  You start the conpetition in the 64 position
+                  That isn't so nice for you.
+                  You have to work hard.
+                  After the first 3 ronds you are in the first 20 players.
+                  and after one more 3 tyou are in the first 10.
+                  At the finis line the are in the 3 position .Good job!
+                  """))
+            return 'not bad'
+
+        elif action =="this is not for you":
+            print(dedent("""
+                  This conpetition it was very hard for you
+                  You are di=o your best:
+                  You earn 5 points ,
+                  and next year you can become campion
+                  In the mind time .
+                  Work hard every single day
+                  and try to do your best
+                  go go go.
+                  """))
+            return'faild'
+
+        else:
+            print("DOES NOT COMPUTE!")
+            return'game_stage'
 
 class RankCv(Stage):
     def enter(self):
@@ -47,10 +112,41 @@ class RankCv(Stage):
         else:
             print ("DOES NOT COMPUTE")
             return 'rank_cv'
+class Result(Stage):
+    def enter(self):
+        print (dedent("""
+             The result of the competition helps you to
+             analuysis your game and
+             to become betterr in the futur .
+              """))
+
+        action = input ("> ")
+
+        if action == "get to work":
+            print (dedent("""
+                  You have to work every singer day
+                  To do three probleme and to play two game
+                  Like that you caan become the next champion of the word.
+                  """))
+            return 'rank_cv'
+
+        elif action == "step by step":
+            print (dedent("""
+                  The game is very simple and verry complex
+                  You have to learn how every pice move and a lot of strategy
+                  The rest is gest matematiques things.
+                  """))
+
+            return 'game'
+        else:
+            print("DOES NOT COMPUTE!")
+            return "price"
 
 class Map(object):
     stages = {
-        'rank_cv': RankCv()
+        'rank_cv': RankCv(),
+        'price':Price(),
+        'game':Game(),
     }
 
     def __init__(self, start_stage):
@@ -62,74 +158,7 @@ class Map(object):
 
     def opening_stage(self):
         return self.next_stage(self.start_stage)
-class Price(Stage):
-    quips = [
-        " You finish the championchip in the 3 position.",
-        " Your Mom would be very proud of you. ",
-        " The result is amaising.",
-        " Because the competiotion was verry difficult.",
-        " Good job! ."
-
-    ]
-    def enter(self):
-        print(Price.quips[randint(0,len(self.quips) -1)])
-        exit(1)
-
-class Game(Stage):
-    def enter(self):
-        print(dedent("""
-             You start the conpetition in the 64 position
-             That isn't so nice for you.
-             You have to work hard.
-             After the first 3 ronds you are in the first 20 players.
-             and after one more 3 tyou are in the first 10.
-             At the finis line the are in the 3 position .Good job!
-             """))
-
-        action = input(">")
-
-        if action == "good!":
-            print(dedent("""
-                  This conpetition it's very hard for you
-                  You after do your best this year:
-                  Try to earn even 5 points ,
-                  and next year you can become campion
-                  In the mind time .
-                  Work hard every single day
-                  and try to do your best
-                  go go go.
-                  """))
-            return'price'
-
-        elif action == "game!":
-            print(dedent("""
-                  You start the conpetition in the 64 position
-                  That isn't so nice for you.
-                  You have to work hard.
-                  After the first 3 ronds you are in the first 20 players.
-                  and after one more 3 tyou are in the first 10.
-                  At the finis line the are in the 3 position .Good job!
-                  """))
-            return 'price'
-
-        elif action =="this is not for you":
-            print(dedent("""
-                  This conpetition it's very hard for you
-                  You after do your best this year:
-                  Try to earn even 5 points ,
-                  and next year you can become campion
-                  In the mind time .
-                  Work hard every single day
-                  and try to do your best
-                  go go go.
-                  """))
-            return'price'
-
-        else:
-            print("DOES NOT COMPUTE!")
-            return'game_stage'
-
 print ("Hello from my game")
-a_map = Map('rank_cv')
+a_map = Map('price')
 a_game = Engine(a_map)
 a_game.play()
