d3465b253444499acf6464e9d233af06a6c28497
lacra-oloeriu <lacramioara.oloeriu@gmail.com>
2017-06-26 04:56:34 +0200

    am refacut dar nu merge

--- projects/skeleton/setup.py
+++ projects/skeleton/setup.py
@@ -4,8 +4,8 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'My Project',
-    'author': 'My Name',
+    'description': 'I learn Python',
+    'author': 'Lacra',
     'url': 'URL to get it at.',
     'download_url': 'Where to download it.',
     'author_email': 'My email.',
