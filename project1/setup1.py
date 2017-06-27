18335602914025c1881ef7c9696b372d50a40405
lacra-oloeriu <lacramioara.oloeriu@gmail.com>
2017-06-27 11:58:28 +0200

    project1
    imcerc sa creez un proiect

---
+++ project1/setup1.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'I learn python',
+    'author': 'Lora',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'lacramioara.oloeriu@gmail.com.',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME1'],
+    'scripts': [],
+    'name': 'project1'
+}
+
+setup(**config)
